5fd8ba6b64e6325da0644a4ab18ae24b24478492
Marco Giammari <marcogiammari@gmail.com>
2023-04-05 14:28:41 +0200

    (for fun) add switch

--- script.js
+++ script.js
@@ -2,5 +2,22 @@ function pwgen() {
     let nome = prompt("Nome:");
     let cognome = prompt("Cognome:");
     let colore = prompt("Qual è il tuo colore preferito?");
+    
     document.getElementById("pw").innerHTML = `${nome}${cognome}${colore}2023`;
+    // document.getElementById("pw").style.backgroundColor = colore;
+    
+    colore = colore.toUpperCase();
+    switch(colore) {
+        case "GIALLO":
+            document.getElementById("pw").style.backgroundColor = "yellow";
+            break
+        case "ROSSO":
+            document.getElementById("pw").style.backgroundColor = "red";
+            break
+        case "BLU":
+            document.getElementById("pw").style.backgroundColor = "blue";
+            break
+        case "VERDE":
+            document.getElementById("pw").style.backgroundColor = "green";
+    }
 }
